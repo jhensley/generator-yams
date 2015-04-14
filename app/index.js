f828eb9fa5f7434ae83188d2e37c9c03fc227a62
Jonathan Hensley <jhensley@gannett.com>
2015-04-14 16:08:42 -0400

    updating some of the prompts

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ var yeoman = require('yeoman-generator'),
     chalk = require('chalk'),
     yosay = require('yosay'),
     path = require('path'),
+    inflections = require('underscore.inflections'),
     _ = require('lodash'),
     _s = require('underscore.string');
 
@@ -20,19 +21,69 @@ module.exports = yeoman.generators.Base.extend({
     ));
 
     var prompts = [{
-      name: 'appName',
+      name: 'appFullName',
       message: 'What do you want to call your amazing new application?',
-      default: "Application of Wonders"
+      default: 'Application of Wonders'
     }, {
         name: 'appDescription',
         message: 'Please provide a description of your application.',
-        default: "This app will do incredible things, including - but not limited to saving the world from mosquitos!"
+        default: 'This app will do incredible things, including - but not limited to saving the world from mosquitos!'
+    }, {
+        type: 'list',
+        name: 'appSiteSpecific',
+        message: 'Will your application manage site-specific data or global?',
+        choices: [{
+            name: 'Site specific fo sho!',
+            value: 'siteSpecificApp'
+        }, {
+            name: 'We goin\' global...',
+            value: 'globalApp'
+        }]
+    }, {
+        type: 'list',
+        name: 'appScope',
+        message: 'Is this application going to be client-side, server-side, or both?',
+        choices: [{
+            name: 'Client-side only',
+            value: 'clientOnlyApp'
+        }, {
+            name: 'Server-side only',
+            value: 'serverOnlyApp'
+        }, {
+            name: 'I want the whole kit and kaboodle!',
+            value: 'clientAndServerApp'
+        }]
+    }, {
+        when: function(props) {
+            return props.appScope.indexOf('serverOnlyApp') === -1
+        },
+        type: 'confirm',
+        name: 'addMenuItem',
+        message: 'Would you like to add an item to the main navigation?'
     }];
 
     this.prompt(prompts, function (props) {
-      this.appname = _s.slugify(props.appName);
-      this.humanReadableName = _s.humanize(props.appName);
+      this.appFullName = props.appFullName;
+      this.appname = _s.slugify(props.appFullName);
+      this.slugifiedName = this.appname;
+      this.slugifiedPluralName = inflections.pluralize(this.slugifiedName);
+      this.slugifiedSingularName = inflections.singularize(this.slugifiedName);
+      this.camelizedPluralName = _s.camelize(this.slugifiedPluralName);
+      this.camelizedSingularName = _s.camelize(this.slugifiedSingularName);
+      this.classifiedPluralName = _s.classify(this.slugifiedPluralName);
+      this.classifiedSingularName = _s.classify(this.slugifiedSingularName);
+      this.humanizedPluralName = _s.humanize(this.slugifiedPluralName);
+      this.humanizedSingularName = _s.humanize(this.slugifiedSingularName);
       this.appDescription = props.appDescription;
+
+      // Site specific vs. global
+      this.siteSpecificApp = props.siteSpecificApp;
+      this.globalApp = props.globalApp;
+
+      // Server vs Client side
+      this.clientOnlyApp = props.clientOnlyApp;
+      this.serverOnlyApp = props.serverOnlyApp;
+      this.clientAndServerApp = props.clientAndServerApp;
 
       done();
     }.bind(this));
