a19999073d407a201b7b4c0bdb2de839916a75aa
Jonathan Hensley <jhensley@gannett.com>
2015-04-16 17:00:08 -0400

    minor tweaks

--- app/templates/public/modules/_/tests/services/_.client.service.test.js
+++ app/templates/public/modules/_/tests/services/_.client.service.test.js
@@ -3,7 +3,7 @@
     // Theme layoutProvider tests
     describe('Service: <%= humanizedPluralName %>', function() {
         var $httpBackend;
-        var ApplicationOfWondersService;
+        var <%= classifiedPluralName %>Service;
         beforeEach(module(ApplicationConfiguration.applicationModuleName));
         beforeEach(inject(function($injector) {
             $httpBackend = $injector.get('$httpBackend');
@@ -11,24 +11,21 @@
                 .respond({data: 'test'});
         }));
         beforeEach(inject(function(_<%= classifiedPluralName %>Service_) {
-        <%= classifiedPluralName %>Service = _<%= classifiedPluralName %>Service_;
+            <%= classifiedPluralName %>Service = _<%= classifiedPluralName %>Service_;
         }));
 
         afterEach(function() {
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
         });
-
-        describe('layoutModulesService', function() {
-            it('Should be defined', function() {
-                expect(<%= classifiedPluralName %>Service).not.toBeUndefined();
+        it('Should be defined', function() {
+            expect(<%= classifiedPluralName %>Service).not.toBeUndefined();
+        });
+        it('Should return the expected data', function() {
+            <%= classifiedPluralName %>Service.get().$promise.then(function(value) {
+                expect(JSON.stringify(value)).toEqual(JSON.stringify({data: 'test'}));
             });
-            it('Should return the expected data', function() {
-                <%= classifiedPluralName %>Service.get().$promise.then(function(value) {
-                    expect(JSON.stringify(value)).toEqual(JSON.stringify({data: 'test'}));
-                });
-                $httpBackend.flush();
-            });
+            $httpBackend.flush();
         });
     });
 }());
